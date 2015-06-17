85bc202b9adf80ce651f0679310b260d1e3e9327
Alexandre Abreu <alexandre.abreu@canonical.com>
2015-06-17 09:46:31 -0400

    Add iojs as an optional node 'implementation'; make search path more flexible

--- cmake/FindNode.cmake
+++ cmake/FindNode.cmake
@@ -1,14 +1,46 @@
-find_program (NODE_EXECUTABLE NAMES node
+# vim:expandtab:shiftwidth=2:tabstop=2:
+
+# Copyright (C) 2015 Canonical Ltd.
+
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+set(NODE_EXECUTABLE_NAMES iojs node)
+
+if (DEFINED ENV{NODE_ROOT_DIR})
+  find_program (NODE_EXECUTABLE NAMES ${NODE_EXECUTABLE_NAMES}
+    PATHS
+    $ENV{NODE_ROOT_DIR}
+    NO_DEFAULT_PATH
+    )
+else($ENV{NODE_ROOT_DIR})
+  find_program (NODE_EXECUTABLE NAMES ${NODE_EXECUTABLE_NAMES}
     HINTS
     $ENV{NODE_DIR}
     PATH_SUFFIXES bin
     DOC "Node.js interpreter"
-)
+    )
+endif()
 
 include (FindPackageHandleStandardArgs)
 
-find_path (NODE_ROOT_DIR "node/node.h" "src/node.h"
-  PATHS /usr/include/nodejs /usr/local/include/nodejs)
+if (NOT DEFINED ENV{NODE_ROOT_DIR})
+    find_path (NODE_ROOT_DIR "node/node.h" "src/node.h"
+      PATHS /usr/include/nodejs /usr/local/include/nodejs)
+else()
+    set(NODE_ROOT_DIR $ENV{NODE_ROOT_DIR})
+endif()
 
 set (NODE_INCLUDE_DIRS
   ${NODE_ROOT_DIR}/src
