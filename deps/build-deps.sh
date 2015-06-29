ef56ffd0c5bc508d621f1a317b352184605330ba
Marcus Tomlinson <marcus.tomlinson@canonical.com>
2015-06-29 09:26:36 +0200

    Don't try to clone deps if directories already exist

--- deps/build-deps.sh
+++ deps/build-deps.sh
@@ -3,8 +3,14 @@
 DIR=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
 
 cd $DIR
-git clone --depth 1 https://chromium.googlesource.com/chromium/tools/depot_tools.git
-git clone --depth 1 --branch 3.28.73 https://chromium.googlesource.com/v8/v8.git
+
+if ! [ -d "depot_tools" ]; then
+    git clone --depth 1 https://chromium.googlesource.com/chromium/tools/depot_tools.git
+fi
+
+if ! [ -d "v8" ]; then
+    git clone --depth 1 --branch 3.28.73 https://chromium.googlesource.com/v8/v8.git
+fi
 
 export PATH=./depot_tools:"$PATH"
 gclient config --spec 'solutions = [
