04c8b9db5a77550efe300f0be053ea14e8009672
Marcus Tomlinson <marcus.tomlinson@canonical.com>
2015-07-01 08:37:38 +0200

    Added more object tests

--- tests/objects/module.cpp
+++ tests/objects/module.cpp
@@ -9,15 +9,29 @@ void InitAll(Handle<Object> exports)
     // Get current isolate
     Isolate* isolate = Isolate::GetCurrent();
 
-    // Prepare class binding
+    // Prepare TestClass binding
     v8cpp::Class<TestClass> testclass(isolate);
     testclass
-            .set_constructor<int, int>();
+            .set_constructor<int, int>()
+            .add_method("i", &TestClass::i)
+            .add_method("embedded_class_ptr", &TestClass::embedded_class_ptr)
+            .add_method("embedded_class_ref", &TestClass::embedded_class_ref)
+            .add_method("embedded_class_copy", &TestClass::embedded_class_copy)
+            .add_method("replace_i", &TestClass::replace_i)
+            .add_method("add_i", &TestClass::add_i);
+
+    // Prepare EmbeddedTestClass binding
+    v8cpp::Class<EmbeddedTestClass> embeddedtestclass(isolate);
+    embeddedtestclass
+            .set_constructor<int, int>()
+            .add_method("i", &EmbeddedTestClass::i);
 
     // Prepare module
     v8cpp::Module module(isolate);
 
     module.add_class("TestClass", testclass);
+    module.add_class("EmbeddedTestClass", embeddedtestclass);
+
     module.add_function("new_TestClass", &new_TestClass);
 
     exports->SetPrototype(module.create_prototype());
--- tests/objects/test.cpp
+++ tests/objects/test.cpp
@@ -9,7 +9,6 @@
 // construct class via factory methods from JS (constructor overloads)
 // move/ref objects to JS
 // move/ref objects back to C++
-// delete class from JS
 
 // METHODS
 // call regular class methods from JS
@@ -37,7 +36,7 @@ TEST(Test, construct_class_via_new)
         test_object;
     )");
 
-    EXPECT_EQ(test_object.i_, 3);
+    EXPECT_EQ(test_object.i(), 3);
 
     isolate->Dispose();
 }
@@ -53,7 +52,72 @@ TEST(Test, construct_class_via_factory)
         test_object;
     )");
 
-    EXPECT_EQ(test_object.i_, 3);
+    EXPECT_EQ(test_object.i(), 3);
+
+    isolate->Dispose();
+}
+
+TEST(Test, object_to_js)
+{
+    v8::Isolate* isolate = v8::Isolate::New();
+
+    auto test_object = v8cpp::run_script<EmbeddedTestClass*>(isolate,
+    R"(
+        var module = require("./module");
+        var test_object = module.new_TestClass(1, 2);
+        test_object.embedded_class_ptr();
+    )");
+
+    EXPECT_EQ(test_object->i(), -1);
+
+    auto test_object2 = v8cpp::run_script<EmbeddedTestClass&>(isolate,
+    R"(
+        var module = require("./module");
+        var test_object = module.new_TestClass(1, 2);
+        test_object.embedded_class_ref();
+    )");
+
+    EXPECT_EQ(test_object2.i(), -1);
+
+    auto test_object3 = v8cpp::run_script<EmbeddedTestClass>(isolate,
+    R"(
+        var module = require("./module");
+        var test_object = module.new_TestClass(1, 2);
+        test_object.embedded_class_copy();
+    )");
+
+    EXPECT_EQ(test_object3.i(), -1);
+
+    isolate->Dispose();
+}
+
+TEST(Test, object_from_js)
+{
+    v8::Isolate* isolate = v8::Isolate::New();
+
+    auto test_object = v8cpp::run_script<TestClass>(isolate,
+    R"(
+        var module = require("./module");
+        var test_object = module.new_TestClass(1, 2);
+        var test_object2 = module.new_TestClass(1, 2);
+
+        test_object.replace_i(test_object2.embedded_class_copy());
+        test_object;
+    )");
+
+    EXPECT_EQ(test_object.i(), -1);
+
+    auto result = v8cpp::run_script<int>(isolate,
+    R"(
+        var module = require("./module");
+        var test_object = module.new_TestClass(1, 2);
+        var test_object2 = module.new_TestClass(1, 2);
+
+        test_object.add_i(test_object.embedded_class_ptr(),
+                          test_object2.embedded_class_ref());
+    )");
+
+    EXPECT_EQ(result, -2);
 
     isolate->Dispose();
 }
