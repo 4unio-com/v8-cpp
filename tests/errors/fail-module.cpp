8e992a6bf02fba8faf3c1b136dcefb73122bfc47
Marcus Tomlinson <marcus.tomlinson@canonical.com>
2015-07-07 12:46:01 +0200

    Build fail-module.cpp as empty lib

--- tests/errors/fail-module.cpp
+++ tests/errors/fail-module.cpp
@@ -1,14 +1,1 @@
-#include <v8-cpp.h>
-
-using namespace v8;
-
-void InitAll(Handle<Object> exports)
-{
-    // Get current isolate
-    Isolate* isolate = Isolate::GetCurrent();
-
-    // Prepare module
-    v8cpp::Module module(isolate);
-
-    exports->SetPrototype(module.create_prototype());
-}
+// Intentionally blank
