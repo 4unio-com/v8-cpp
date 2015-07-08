9dd2d9a2638466570015e73d6c3a3bdc43935fc3
Marcus Tomlinson <marcus.tomlinson@canonical.com>
2015-07-08 08:20:26 +0200

    Added missing copyright notices

--- example/example.js
+++ example/example.js
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2015 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Marcus Tomlinson <marcus.tomlinson@canonical.com>
+ */
+
 var addon = require('./v8-cpp-example');
 
 var obj = addon.new_MyObject(10);
