8d58a78702d15338d472e202cab5eff03184c2e7
Marcus Tomlinson <marcus.tomlinson@canonical.com>
2015-07-29 10:55:01 +0200

    Add locker to convert.h

--- src/convert.h
+++ src/convert.h
@@ -32,6 +32,7 @@ namespace v8cpp
 template <typename T>
 typename internal::Convert<T>::FromType from_v8(v8::Isolate* isolate, v8::Handle<v8::Value> value)
 {
+    internal::Locker l(isolate);
     return internal::Convert<T>::from_v8(isolate, value);
 }
 
@@ -40,29 +41,34 @@ typename internal::Convert<T>::FromType from_v8(v8::Isolate* isolate,
                                                 v8::Handle<v8::Value> value,
                                                 U const& default_value)
 {
+    internal::Locker l(isolate);
     return internal::Convert<T>::is_valid(isolate, value) ? internal::Convert<T>::from_v8(isolate, value) :
                                                             default_value;
 }
 
 inline v8::Handle<v8::String> to_v8(v8::Isolate* isolate, char const* str, int len = -1)
 {
+    internal::Locker l(isolate);
     return v8::String::NewFromUtf8(isolate, str, v8::String::kNormalString, len);
 }
 
 template <typename T>
 typename internal::Convert<T>::ToType to_v8(v8::Isolate* isolate, T const& value)
 {
+    internal::Locker l(isolate);
     return internal::Convert<T>::to_v8(isolate, value);
 }
 
 inline v8::Handle<v8::Value> to_v8(v8::Isolate* isolate, std::nullptr_t)
 {
+    internal::Locker l(isolate);
     return v8::Null(isolate);
 }
 
 template <typename I>
 v8::Handle<v8::Array> to_v8(v8::Isolate* isolate, I begin, I end)
 {
+    internal::Locker l(isolate);
     v8::EscapableHandleScope scope(isolate);
 
     v8::Local<v8::Array> result = v8::Array::New(isolate);
@@ -76,6 +82,7 @@ v8::Handle<v8::Array> to_v8(v8::Isolate* isolate, I begin, I end)
 template <typename T>
 v8::Local<T> to_local(v8::Isolate* isolate, v8::PersistentBase<T> const& handle)
 {
+    internal::Locker l(isolate);
     if (handle.IsWeak())
     {
         return v8::Local<T>::New(isolate, handle);
