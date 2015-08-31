3c167bee0563f5790e51196e5ff02ccd523d8ed0
Marcus Tomlinson <marcus.tomlinson@canonical.com>
2015-08-31 06:13:15 +0200

    Fixed deps retrieval

--- deps/build-deps.sh
+++ deps/build-deps.sh
@@ -4,14 +4,6 @@ DIR=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
 
 cd $DIR
 
-if ! [ -d "depot_tools" ]; then
-    git clone --depth 1 https://chromium.googlesource.com/chromium/tools/depot_tools.git
-fi
-
-if ! [ -d "v8" ]; then
-    git clone --depth 1 --branch 3.28.73 https://chromium.googlesource.com/v8/v8.git
-fi
-
 export PATH=./depot_tools:"$PATH"
 gclient config --spec 'solutions = [
   { "name"        : "v8",
